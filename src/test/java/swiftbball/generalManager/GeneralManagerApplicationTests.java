a9befbbd598b9ead9008838298d4a4793df85a42
Notso <generalemail7@yahoo.com>
2022-09-29 00:45:48 -0500

    Spring Initialized app

---
+++ src/test/java/swiftbball/generalManager/GeneralManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package swiftbball.generalManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
